dc68a33718e5a0d820e9542194f2fef685a58c32
ihcomega56 <ihcomega56@gmail.com>
2015-12-07 00:42:05 +0900

    インデントが

--- JavaWomap/ViewController.swift
+++ JavaWomap/ViewController.swift
@@ -23,24 +23,24 @@ class ViewController: UIViewController {
         self.view = mapView
         
         func addMarker(longitude: Double, latitude: Double, caption: String, population: Int, icon: UIImage?) {
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(longitude, latitude)
-        marker.title = caption
-        marker.snippet = population.description + "名"
-        
-        marker.map = mapView
-        marker.icon = icon
+            let marker = GMSMarker()
+            marker.position = CLLocationCoordinate2DMake(longitude, latitude)
+            marker.title = caption
+            marker.snippet = population.description + "名"
+            
+            marker.map = mapView
+            marker.icon = icon
         }
         
         addMarker(35.689634, latitude: 139.692101, caption: "Java女子部(関東)", population: 290, icon: tokyoIcon)
         addMarker(34.686297, latitude: 135.519661, caption: "関西Java女子部", population: 60, icon: kansaiIcon)
         addMarker(33.606576, latitude: 130.418297, caption: "九州Java女子部", population: 1, icon: kyushuIcon)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 }
 
